137215b39c3dccefedda0e876a3d1fe819eaa113
ecwright3 <ecwright3@gmail.com>
2017-08-06 08:04:42 -0400

    fixed issue with the newline character being included in showtime events. Added Theater name, address and phone number to each movie entry.

--- scrap-pile/movienight/movienight/spiders/regal_spider.py
+++ scrap-pile/movienight/movienight/spiders/regal_spider.py
@@ -6,21 +6,31 @@ class RegalSpider (scrapy.Spider):
 
 
     start_urls = [
-        #'https://www.regmovies.com/theaters/regal-westview-stadium-16-imax/8341',
-        'https://www.regmovies.com/theaters/regal-germantown-stadium-14/8459'
+        'https://www.regmovies.com/theaters/regal-westview-stadium-16-imax/8341',
+        #'https://www.regmovies.com/theaters/regal-germantown-stadium-14/8459'
     ] 
 
     def parse(self, response):
+        
         for movie in response.css('ul.showtime-panel-list > li'):
-            
+            theater = response.css('div.page-header > div.info-cell')
+            #remove the \n from showtimes list 
+            times = list(map(str.strip, movie.css('li.showtime-entry a::text').extract()))
+
+
+
             data = movie.css('h3.title a::attr(data-csm)').extract_first()
-            movieinfo = json.loads(data) 
+            movieinfo = json.loads(data)
             yield {
                 'title' : movieinfo['productionName'],
                 'productionid' : movieinfo['productionId'],
                 'rating' : movie.css('ul.list-inline title::text').extract_first(),
                 'duration' : movie.css('ul.list-inline li::text').extract()[2],
-                'showtimes' : movie.css('li.showtime-entry a::text').extract(), # The number of showtimes will change as the day passes. past show times will drop off.  
+                #'showtimes' : movie.css('normalize-space(li.showtime-entry a::text)').extract()., # The number of showtimes will change as the day passes. past show times will drop off.
+                'showtimes': times,
+                'theater' : theater.css('h1.title::text').extract_first(),
+                'address' : theater.css('div.address > a::text').extract_first(),
+                'phone' : theater.css('div.phone::text').extract_first().strip(),
             }
 
         
